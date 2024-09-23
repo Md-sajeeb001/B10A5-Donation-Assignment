6e4293baebcc563912dc0c8388078ce74b4b7a14
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-23 16:37:50 +0800

    added javascript

---
+++ js/index.js
@@ -0,0 +1,62 @@
+// uitilitis part !
+
+function getInputValueById(id){
+    return parseFloat(document.getElementById(id).value);
+}
+function getTextValueById(id){
+    return parseFloat(document.getElementById(id).innerText);
+}
+
+// ======================================================================== //
+
+document.getElementById('modelBtn').addEventListener('click',function(){
+    const inputValue = getInputValueById('donateInputValue');
+    const CurrentBalance = getTextValueById('current-balance');
+    const btnValue = getTextValueById('addDonationNoakhali');
+
+    const decreseBalance = CurrentBalance -inputValue;
+    document.getElementById('current-balance').innerText = decreseBalance;
+
+    const addingDonate = btnValue + inputValue;
+    document.getElementById('addDonationNoakhali').innerText = addingDonate;
+    
+
+});
+
+document.getElementById('modelBtn2').addEventListener('click', function(){
+    const feniInputValue = getInputValueById('inputValueFeni');
+    const CurrentBalanceFeni = getTextValueById('current-balance');
+    const btnValueFeni = getTextValueById('feniAddBtn');
+
+    const decreseBalance = CurrentBalanceFeni - feniInputValue;
+    document.getElementById('current-balance').innerText = decreseBalance;
+
+    const addingDonate = btnValueFeni + feniInputValue;
+    document.getElementById('feniAddBtn').innerText = addingDonate;
+
+});
+
+document.getElementById('modelBtn3').addEventListener('click', function(){
+    const injuredMovmentInput = getInputValueById('InjuredMovment');
+    const CurrentBalanceInjured = getTextValueById('current-balance');
+    const btnValueInjured = getTextValueById('InjuredMovmentBtn');
+
+    const decreseBalance = CurrentBalanceInjured - injuredMovmentInput;
+    document.getElementById('current-balance').innerText = decreseBalance;
+    
+    const addingDonate = injuredMovmentInput + btnValueInjured;
+    document.getElementById('InjuredMovmentBtn').innerText = addingDonate;
+
+})
+
+// Blog page !
+
+document.getElementById('blogPage').addEventListener('click', function(){
+    const blogPage = document.getElementById('mainPage');
+    blogPage.classList.add('hidden');
+
+   document.getElementById('blogSection').classList.remove('hidden')
+
+
+})
+
