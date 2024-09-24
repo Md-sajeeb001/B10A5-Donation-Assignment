392ba876f6b360f92c657f9eaf502f18a5fa2162
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-24 22:30:58 +0800

    added navber section

--- js/index.js
+++ js/index.js
@@ -16,6 +16,9 @@ document.getElementById('modelBtn1').addEventListener('click', function(){
        return  alert("Please provid a valid input")
     }
     const CurrentBalance = getTextValueById('current-balance');
+    if(inputValue > CurrentBalance){
+       return alert('Your do not have enough money to donate!')
+    }
     const btnValue = getTextValueById('addDonationNoakhali');
 
     const decreseBalance = CurrentBalance -inputValue;
@@ -31,9 +34,9 @@ document.getElementById('modelBtn1').addEventListener('click', function(){
     createDivEL.innerHTML = `
     
     <section class="lg:w-10/12 w-full mx-auto mb-4">
-        <div class="shadow-lg rounded-lg px-8 py-6 space-y-5 border-2">
-            <p class="text-lg font-semibold">${inputValue} Taka is Donated for Donate for flood at Nowakhali, Bangladesh</p>
-            <p class="text-secondaryText">${new Date().toLocaleString()}</p>
+        <div class="shadow-md border border-slate-200 rounded-lg lg:px-8 py-6 space-y-5 mx-3 px-5">
+            <p class="text-lg font-semibold">${inputValue} Taka is Donated for Donate for flood at Noakhali, Bangladesh</p>
+            <p class="text-secondaryText lg:bg-white bg-slate-300 lg:px-0 lg:py-0 px-6 py-4">${new Date().toLocaleString()}</p>
         </div>
     </section>
     `
@@ -47,6 +50,9 @@ document.getElementById('modelBtn2').addEventListener('click', function(){
         return  alert("Please provid a valid input")
      }
     const CurrentBalanceFeni = getTextValueById('current-balance');
+    if(feniInputValue > CurrentBalanceFeni){
+        return alert('Your do not have enough money to donate!')
+     }
     const btnValueFeni = getTextValueById('feniAddBtn');
 
     const decreseBalance = CurrentBalanceFeni - feniInputValue;
@@ -61,9 +67,9 @@ document.getElementById('modelBtn2').addEventListener('click', function(){
     createDivEL.innerHTML = `
     
     <section class="lg:w-10/12 w-full mx-auto mb-4">
-        <div class="shadow-lg rounded-lg px-8 py-6 space-y-5 border-2">
+        <div class="shadow-md border border-slate-200 rounded-lg lg:px-8 py-6 space-y-5 mx-3 px-5">
             <p class="text-lg font-semibold">${feniInputValue} Taka is Donated for Donate for flood at Feni, Bangladesh</p>
-            <p class="text-secondaryText">${new Date().toLocaleString()}</p>
+            <p class="text-secondaryText lg:bg-white bg-slate-300 lg:px-0 lg:py-0 px-6 py-4">${new Date().toLocaleString()}</p>
         </div>
     </section>
     `
@@ -77,6 +83,9 @@ document.getElementById('modelBtn3').addEventListener('click', function(){
         return  alert("Please provid a valid input")
      }
     const CurrentBalanceInjured = getTextValueById('current-balance');
+    if(injuredMovmentInput > CurrentBalanceInjured){
+        return alert('Your do not have enough money to donate!')
+     }
     const btnValueInjured = getTextValueById('InjuredMovmentBtn');
 
     const decreseBalance = CurrentBalanceInjured - injuredMovmentInput;
@@ -91,9 +100,9 @@ document.getElementById('modelBtn3').addEventListener('click', function(){
     createDivEL.innerHTML = `
     
     <section class="lg:w-10/12 w-full mx-auto mb-4">
-        <div class="shadow-lg rounded-lg px-8 py-6 space-y-5 border-2">
-            <p class="text-lg font-semibold">${injuredMovmentInput} Taka is Donated for Donate for flood at Bangladesh</p>
-            <p class="text-secondaryText">${new Date().toLocaleString()}</p>
+        <div class="shadow-md border border-slate-200 rounded-lg lg:px-8 py-6 space-y-5 mx-3 px-5">
+            <p class="text-lg font-semibold">${injuredMovmentInput} Taka is Donated for Donate for flood at, Bangladesh</p>
+            <p class="text-secondaryText lg:bg-white bg-slate-300 lg:px-0 lg:py-0 px-6 py-4">${new Date().toLocaleString()}</p>
         </div>
     </section>
     `
